81e93e8dccf744102e1681e6682526e22f3e8ead
suvarnars17 <suvarnars17@gmail.com>
2022-12-16 17:28:28 +0530

    first commit

---
+++ force-app/main/default/lwc/containerCmp/containerCmp.js
@@ -0,0 +1,8 @@
+import { LightningElement } from 'lwc';
+
+export default class ContainerCmp extends LightningElement {
+    bike ={
+        name :'Hero',
+        company :'Maruti Show Room'
+    }
+}
